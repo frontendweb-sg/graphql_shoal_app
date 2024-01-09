6a95c69c10670d85e85b042194bac0e7d24bf220
Amit Chopra <amit.chopra@rsystems.com>
2024-01-09 10:55:17 +0530

    controls

--- lib/features/history/presentation/pages/history_screen.dart
+++ lib/features/history/presentation/pages/history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_shoal_app/shared/widgets/button.dart';
 import 'package:graphql_shoal_app/shared/widgets/calender.dart';
 import 'package:graphql_shoal_app/shared/widgets/dropdown.dart';
 import 'package:graphql_shoal_app/shared/widgets/switchbutton.dart';
@@ -11,10 +12,21 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
-  bool checkBoxValue = false;
-  String radioBtnValue = "male";
+
+  bool checkBoxValue = true;
+  int radioBtnValue = 2;
+  bool switchButtonValue = true;  
+
+  final _formKey = GlobalKey<FormState>();
+    final Map<String, dynamic> formelements = {"Gender": false, "SelectedGender": 1, "Terms": true, "title": "Amit"};
+
+  void onSubmit() async {
+    print(formelements);
+  }
+
   String defultDropDownValue =  "Amit";
-  List<Map<String, dynamic>> dropDownItems = [
+  
+  final dropDownItems = [
     {"title": "Amit", "value": "1"},
     {"title": "Vinay", "value": "2"},
     {"title": "Pradeep", "value": "3"},
@@ -41,7 +53,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
     'Item 5', 
   ];
 
-  bool switchButtonValue = false;  
+ 
 
   TextEditingController _textEditingController = TextEditingController();  
 
@@ -50,124 +62,147 @@ class _HistoryScreenState extends State<HistoryScreen> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-            child: Column(
-          children: [
-
-            CheckboxListTile(
-                contentPadding: const EdgeInsets.all(0.0),
-                controlAffinity: ListTileControlAffinity.leading,
-                title: const Text("Male"),
-                value: checkBoxValue,
-                onChanged: (value) {
-                  setState(() {
-                    checkBoxValue = value!;
-                  });
-                }),
-
-            const SizedBox(height: 30.0),
-
-            Row(children: [
-              Expanded(
-                child: RadioListTile(
+            child: Form(
+              key: _formKey,
+              child: Column(
+              children: [
+              
+              CheckboxListTile(
                   contentPadding: const EdgeInsets.all(0.0),
                   controlAffinity: ListTileControlAffinity.leading,
                   title: const Text("Male"),
-                  value: "Male",
-                  groupValue: radioBtnValue,
+                  value: formelements['Gender'],
                   onChanged: (value) {
                     setState(() {
-                      radioBtnValue = value!;
+                      formelements['checkBoxValue'] = value!;
                     });
-                  },
+                  }),
+              
+              const SizedBox(height: 30.0),
+              
+              Row(children: [
+                Expanded(
+                  child: RadioListTile(
+                    contentPadding: const EdgeInsets.all(0.0),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: const Text("Male"),
+                    value: 1,
+                    groupValue: formelements['SelectedGender'],
+                    onChanged: (value) {
+                      setState(() {
+                        formelements['radioBtnValue'] = value!;
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Expanded(
-                child: RadioListTile(
-                  contentPadding: const EdgeInsets.all(0.0),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: const Text("Female"),
-                  value: "Female",
-                  groupValue: radioBtnValue,
-                  onChanged: (value) {
+                Expanded(
+                  child: RadioListTile(
+                    contentPadding: const EdgeInsets.all(0.0),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: const Text("Female"),
+                    value: 2,
+                    groupValue: formelements['SelectedGender'],
+                    onChanged: (value) {
+                      setState(() {
+                        formelements['radioBtnValue'] = value!;
+                      });
+                    },
+                  ),
+                ),
+              ]),
+
+
+              const SizedBox(height: 30.0),              
+                switchButton(
+                  context, 
+                  title: "Terms",
+                  value: formelements['Terms'], 
+                  onChanged: (value){
                     setState(() {
-                      radioBtnValue = value!;
+                      formelements['switchButtonValue'] = value;
                     });
-                  },
-                ),
-              ),
-            ]),
+                  }),
+              
+              const SizedBox(height: 30.0),
+              
+              DropdownButtonHideUnderline(
+                      child: DropdownButton<Object>(
+                        isExpanded: true,
+                        isDense: true,
+                        menuMaxHeight: 350,
+                        value: formelements['title'],
+                        items:dropDownItems.map((data) {
+                          return DropdownMenuItem(
+                            value: data['value'],
+                            child: Text(data['title']),
+                          );
+                        }).toList(),
+                        onChanged:(selectedValue) {
+                          setState(() {
+                            selectedValue
+                          });
+                        }                       
+                      ),
+                    ),
+              
+              const SizedBox(height: 30.0),
+              
+              // calendar(
+              //   context,
+              //   labelText: "DOB", 
+              //   controller: _textEditingController,
+              //   ),
+              
+            
+              
+              //  const SizedBox(height: 30.0),   
+              
+              //   Autocomplete<String>(              
+              //     optionsBuilder: (TextEditingValue fruitTextEditingValue) { 
+              //       if (fruitTextEditingValue.text == '') { 
+              //         return const Iterable<String>.empty(); 
+              //       } 
+              //       return _fruitOptions.where((String option) { 
+              //         return option 
+              //             .contains(fruitTextEditingValue.text.toLowerCase()); 
+              //       }); 
+              //     }, 
+              //       onSelected: (String value) { 
+              //         debugPrint('You just selected $value'); 
+              //       }, 
+              //     ), 
+              
+              //     const SizedBox(height: 30.0),  
+              
+              //     DropdownButton( 
+                  
+              //       value: dropdownvalue, 
+              //       icon: const Icon(Icons.keyboard_arrow_down),     
+              //       items: dropitems.map((String items) { 
+              //         return DropdownMenuItem( 
+              //           value: items, 
+              //           child: Text(items), 
+              //         ); 
+              //       }).toList(), 
+              
+              //         onChanged: (String? newValue) {  
+              //           setState(() { 
+              //             dropdownvalue = newValue!; 
+              //           }); 
+              //         }, 
+              // ), 
 
-            const SizedBox(height: 30.0),
+                const SizedBox(height: 30.0),
 
-            dropDownList(
-              context,
-              items: dropDownItems,
-              value: defultDropDownValue,
-              onChanged: (title) {
-                setState(() {
-                  defultDropDownValue = title.toString();
-                });
-              },              
-              ),
+                    button(
+                      context,
+                      label: "Submit",
+                      onPressed: onSubmit,                    
+                    ),
 
-            const SizedBox(height: 30.0),
-
-            calendar(
-              context,
-              labelText: "DOB", 
-              controller: _textEditingController,
-              ),
-
-             const SizedBox(height: 30.0),
-
-              switchButton(
-                context, 
-                title: "Male",
-                value: switchButtonValue, 
-                onChanged: (value){
-                  setState(() {
-                    switchButtonValue = value;
-                  });
-                }),
-
-             const SizedBox(height: 30.0),   
-
-              Autocomplete<String>(              
-                optionsBuilder: (TextEditingValue fruitTextEditingValue) { 
-                  if (fruitTextEditingValue.text == '') { 
-                    return const Iterable<String>.empty(); 
-                  } 
-                  return _fruitOptions.where((String option) { 
-                    return option 
-                        .contains(fruitTextEditingValue.text.toLowerCase()); 
-                  }); 
-                }, 
-                  onSelected: (String value) { 
-                    debugPrint('You just selected $value'); 
-                  }, 
-                ), 
-
-                const SizedBox(height: 30.0),  
-
-                DropdownButton( 
-                
-                  value: dropdownvalue, 
-                  icon: const Icon(Icons.keyboard_arrow_down),     
-                  items: dropitems.map((String items) { 
-                    return DropdownMenuItem( 
-                      value: items, 
-                      child: Text(items), 
-                    ); 
-                  }).toList(), 
-
-                    onChanged: (String? newValue) {  
-                      setState(() { 
-                        dropdownvalue = newValue!; 
-                      }); 
-                    }, 
-            ), 
-          ],
-        )),
+                        ],
+                      ),
+            )),
       ),
     );
   }
